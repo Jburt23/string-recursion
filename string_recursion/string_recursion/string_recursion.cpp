3dd7bf059c4ecd19d0dce0e6284dec3c36896433
Justin Burt <jstnbrt@hotmail.com>
2018-04-18 15:38:34 -0500

    All of this work just to print this assignment off

---
+++ string_recursion/string_recursion/string_recursion.cpp
@@ -0,0 +1,38 @@
+//Justin Burt
+//CSC 2144----------Period CN1
+//April 18th, 2018--Spring 2018
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void recursion(string);
+int i = 1; 
+
+int main()
+{
+	string myString;
+
+	cout << "Please enter in a sentence or word: ";
+	getline(cin, myString);
+
+	recursion(myString);
+	cout << endl;
+
+	system("PAUSE");
+	return 0;
+}
+
+void recursion(string myString2)
+{
+
+	if  (i <= size(myString2))
+	{
+		cout << myString2[size(myString2) - i];
+		i++;
+		recursion(myString2);
+	}
+	
+	
+}
